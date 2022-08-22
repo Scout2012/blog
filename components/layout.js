15ecb91e8c3647ebba7ff7058226e069182913d0
Jacob Powell <jcbpowell12@gmail.com>
2022-08-22 01:41:17 -0700

    adding image optimizer

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+import ExportedImage from "next-image-export-optimizer";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
@@ -22,7 +22,7 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
-            <Image
+            <ExportedImage
               priority
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
@@ -36,7 +36,7 @@ export default function Layout({ children, home }) {
           <>
             <Link href="/">
               <a>
-                <Image
+                <ExportedImage 
                   priority
                   src="/images/profile.jpg"
                   className={utilStyles.borderCircle}
