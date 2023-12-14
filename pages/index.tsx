ae6ffe97ca72237a318b15245e6d98efd5d58c00
Jacob Powell <jcbpowell12@gmail.com>
2023-12-14 01:47:05 -0800

    better fix for weird padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export async function getStaticProps() {
 export default function Home() {
   return (
     <Layout>
-      <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <div style={{textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center"}}>
         <p>please enjoy your stay</p>
         <p>the library is always a good place to go when you're unsure what to do</p>
       </div>
